8b87576779b8be0be1ed314326e783258f0d9a43
Jesus Correa <jcorrea2@mail.sfsu.edu>
2022-12-27 20:29:11 -0800

    fixed the cards for the battlers. I changed the css so that the card looks more appealing and also made the image centered in the background. The battler info might need more work so it looks better but the card overall looks a lot better with the 3D looking effect and the hover effect.

--- battle_rap/components/BattleCard.js
+++ battle_rap/components/BattleCard.js
@@ -8,19 +8,24 @@ export const BattleCard = (props) => {
 
     return(
         <div className="card-container">
-            <div className="img-container">
-                <Image className="battle-card-img" src={props.picPath} alt="Picture does not work" width={100} height= {300}/>
-                {/* <img src={props.picPath} className ="battle-card-img"  alt="Picture does not work" /> */}
-            </div>
+            {/* <div className="img-container"> */}
+                {/* <Image className="battle-card-img" src={props.picPath} alt="Picture does not work" width={1000} height= {618}/> */}
+                {/* <img src={props.picPath} className ="battle-card-img"  alt="Picture does not work"/> */}
+            {/* </div> */}
             
-            <div className='name-container'>
-                <h2>Battler: {props.name}</h2>
-            </div>
-            <div className="numberBattles-container">
-                <p>Battles: {parseInt(props.numberBattles)}</p>
-            </div>
-            <div className="views-container">
-                <p>Views: {parseInt(props.views)}</p>
+            <div className= 'battler-info'>
+                <div className="info-background">
+                    <div className='name-container info'>
+                        <h2>Battler: {props.name}</h2>
+                    </div>
+                    <div className='numberBattles-container info'>
+                        <p>Battles: {parseInt(props.numberBattles)}</p>
+                    </div>
+                    <div className="views-container info">
+                        <p>Views: {parseInt(props.views)}</p>
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
--- battle_rap/pages/battlers.js
+++ battle_rap/pages/battlers.js
@@ -2,12 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Navbar } from '../components/Navbar.js'
-import { BattleCard } from '../components/BattleCard'
+import { BattleCard } from '../components/BattleCard.js'
 
 export const Battlers = () => {
     return(
         <>
-            <BattleCard picPath='/../public/jesus.png' name="Jesus" views="390" numberBattles ="4"></BattleCard>
+            <BattleCard picPath='/banana.jpeg' name="Jesus" views="390" numberBattles ="4"/>
         </>
         
 
