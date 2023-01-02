b37dda9bd582e0b0faa292846220a0cd792038d4
Jesus Correa <jcorrea2@mail.sfsu.edu>
2023-01-01 16:34:55 -0800

    Created functions for the database module. We can now import its functions to a new module. We can now create functions to create and end database connections

--- battle_rap/pages/api/db.js
+++ battle_rap/pages/api/db.js
@@ -1,26 +1,55 @@
 import mysql from 'mysql2/promise'
 
-export default async function handler(req, res){
 
-    const dbConnection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            database: 'battle_rap',
-            password: 'YE2n4qh4wV',
-            port: '3306',
-    });
+
+export default async function handler(req, res){
+    
+    var dbConnection = await connectDb();
 
     try{
-        const query = "select * from battlers";
+        const query = "select * from battlers where name = 'Z'";
         const values = [];
+
+        console.log("About to query");
+
         const [data] = await dbConnection.execute(query, values);
-        dbConnection.end();
+        endDbConnect(dbConnection);
+
+        // if(JSON.stringify(data) === '{}'){
+        //     console.log("The data is not empty");
+        // }else{
+        //     console.log("the data is empty");
+        // }
+
         res.status(200).json({results: data});
 
     }catch(error){
-        res.status(500).json({results: data});
+        res.status(500).json({results: "You not good bro"});
     }
 
 }
 
-// modules.export = dbConnection;
+
+export async function connectDb(){
+    // console.log("In the connectDB function");
+    var dbConnection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'battle_rap',
+        password: 'YE2n4qh4wV',
+        port: '3306',
+    });
+    
+
+    // res.status(200).json({message: "Connection was made"});
+    return dbConnection;
+}
+
+export function endDbConnect(connection){
+    connection.end();
+}
+
+// async function ()
+// modules.exports = connectDb;
+
+
--- battle_rap/pages/api/populate.js
+++ battle_rap/pages/api/populate.js
@@ -1,19 +1,84 @@
-// import {dbConnection} from "/api/db.js"
+var db = require('./db.js')
+
+var pageKey = "";
 
 
 export default async function getAPIresults(req, res){
     const apiKey = "AIzaSyAFSDkW1DnyLDEqnBUI0TKVz5WxG8BXKss";
+    const order = "viewCount";
+    const maxResults = "1";
+    const channelId = "UCflIAeM03JFL9ml03LwYF-g";
+    const part = "snippet";
+    var endPoint = null;
+    var vidIds = [];
+    
 
     try{
-        const response = await fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAFSDkW1DnyLDEqnBUI0TKVz5WxG8BXKss&channelId=UCflIAeM03JFL9ml03LwYF-g&maxResults=5&order=viewCount');
-        const data = await response.json();
-        console.log(data);
+        // do{
+            
+        // }while(pageKey !== null)
+
+        if(pageKey !== null){
+            endPoint = `https://www.googleapis.com/youtube/v3/search?part=${part}&key=${apiKey}&channelId=${channelId}&maxResults=${maxResults}&order=${order}&pageToken=${pageKey}`
+        }else{
+            endPoint = `https://www.googleapis.com/youtube/v3/search?part=${part}&key=${apiKey}&channelId=${channelId}&maxResults=${maxResults}&order=${order}`
+        }
+
+        //Get the data from the Youtube API and convert to JSON
+        const response = await fetch(endPoint);
+        const data = await response.json();  
+
+        //Get the video IDs from the information so that we can get info for each battler
+        vidIds = getVidIds(data, vidIds);
+        // getNames(data);
+        // vidIds.push(...getVidIds(data, vidIds));
+        
+        
+
+        // console.log(pageKey);
         res.status(200).json({results: data});
+        console.log(data);
+
 
     }catch(error){
-        res.status(500).json({bruh: "error"});
+        res.status(500).json({error:error.message});
     }
+
     
+    
+}
+
+// function getNames(data){
+//     const vidTitle = data.items[0].snippet.title;
+
+//     // Split the input string by "VS"
+//     const nameArray = vidTitle.split("VS");
+
+//     // Trim leading and trailing whitespace from the two names
+//     const name1 = nameArray[0].trim();
+
+//     // Split the second name by the first space character
+//     const name2Array = nameArray[1].split(" ");
+
+//     // The first element in the resulting array is the second name
+//     const name2 = name2Array[1].trim();
+
+//     console.log(name1);  // Output: "LOADED LUX"
+//     console.log(name2);  // Output: "CHARLIE CLIPS"
+
+// }
+
+async function getVidIds(data, vidIds){
+    const itemObjs = data.items;
+    
+    itemObjs.map(obj => vidIds.push(obj.id.videoId));
+
+    if(data.nextPageToken !== null){
+        pageKey = data.nextPageToken;
+    }
+
+    return vidIds;
+
 }
 
 export async function getVideoViews(videoId){
